0595aea42a29af289278a8327eb1627f177c1505
Ridhdhi-Avaiya <avaiyaridhdhi@gmail.com>
2022-08-03 09:53:43 +0530

    Firebase fill chaged

--- src/firebase.js
+++ src/firebase.js
@@ -5,15 +5,7 @@ import { getStorage } from 'firebase/storage'
 import { getFirestore } from 'firebase/firestore'
 import 'firebase/storage'
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyDildmT7gCxr8RENg1rbHW9hwQvD-fac00',
-  authDomain: 'chat-app-6e3f1.firebaseapp.com',
-  projectId: 'chat-app-6e3f1',
-  storageBucket: 'chat-app-6e3f1.appspot.com',
-  messagingSenderId: '624261162783',
-  appId: '1:624261162783:web:6339c27ee2af13dee6722a',
-  measurementId: 'G-ZJEGZRZG9M',
-}
+const firebaseConfig = {}
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
