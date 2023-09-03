9fdda17a8c04b8cb3530c4449d26cf9977018275
rahulsan007 <santrarahul7070@gmail.com>
2023-09-03 14:20:49 +0530

    language ask

--- src/pages/Chat/Chat.jsx
+++ src/pages/Chat/Chat.jsx
@@ -1,10 +1,10 @@
-import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import MuiDrawer from '@mui/material/Drawer';
-import MuiAppBar from '@mui/material/AppBar';
-import CssBaseline from '@mui/material/CssBaseline';
-import Divider from '@mui/material/Divider';
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import MuiDrawer from "@mui/material/Drawer";
+import MuiAppBar from "@mui/material/AppBar";
+import CssBaseline from "@mui/material/CssBaseline";
+import Divider from "@mui/material/Divider";
 import {
   Avatar,
   Badge,
@@ -16,9 +16,14 @@ import {
   InputLabel,
   OutlinedInput,
   Typography,
-} from '@mui/material';
-import { useUserAuth } from '../../context/userAuthContext';
-import TelegramIcon from '@mui/icons-material/Telegram';
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  Button,
+} from "@mui/material";
+import { useUserAuth } from "../../context/userAuthContext";
+import TelegramIcon from "@mui/icons-material/Telegram";
 import {
   collection,
   query,
@@ -30,119 +35,120 @@ import {
   doc,
   getDoc,
   setDoc,
-} from 'firebase/firestore';
-import { db } from '../../firebase';
-import { DrawerWithNav } from './components/DrawerWithNav';
-import moment from 'moment';
-import './chat.css';
-import { useDrawer } from '../../context/drawerContext';
+} from "firebase/firestore";
+import { db } from "../../firebase";
+import { DrawerWithNav } from "./components/DrawerWithNav";
+import moment from "moment";
+import "./chat.css";
+import { useDrawer } from "../../context/drawerContext";
 
 const drawerWidth = 240;
 
 const openedMixin = (theme) => ({
   width: drawerWidth,
-  transition: theme.transitions.create('width', {
+  transition: theme.transitions.create("width", {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.enteringScreen,
   }),
-  overflowX: 'hidden',
+  overflowX: "hidden",
 });
 
 const closedMixin = (theme) => ({
-  transition: theme.transitions.create('width', {
+  transition: theme.transitions.create("width", {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.leavingScreen,
   }),
-  overflowX: 'hidden',
+  overflowX: "hidden",
   width: `calc(${theme.spacing(7)} + 1px)`,
-  [theme.breakpoints.up('sm')]: {
+  [theme.breakpoints.up("sm")]: {
     width: `calc(${theme.spacing(8)} + 1px)`,
   },
 });
 
-export const DrawerHeader = styled('div')(({ theme }) => ({
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'flex-end',
+export const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "flex-end",
   padding: theme.spacing(0, 1),
   // necessary for content to be below app bar
   ...theme.mixins.toolbar,
 }));
 
 export const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== 'open',
+  shouldForwardProp: (prop) => prop !== "open",
 })(({ theme, open }) => ({
   zIndex: theme.zIndex.drawer + 1,
-  transition: theme.transitions.create(['width', 'margin'], {
+  transition: theme.transitions.create(["width", "margin"], {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.leavingScreen,
   }),
   ...(open && {
     marginLeft: drawerWidth,
     width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(['width', 'margin'], {
+    transition: theme.transitions.create(["width", "margin"], {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.enteringScreen,
     }),
   }),
 }));
 
-export const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
-  ({ theme, open }) => ({
-    width: drawerWidth,
-    flexShrink: 0,
-    whiteSpace: 'nowrap',
-    boxSizing: 'border-box',
-    ...(open && {
-      ...openedMixin(theme),
-      '& .MuiDrawer-paper': openedMixin(theme),
-    }),
-    ...(!open && {
-      ...closedMixin(theme),
-      '& .MuiDrawer-paper': closedMixin(theme),
-    }),
-  })
-);
+export const Drawer = styled(MuiDrawer, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  width: drawerWidth,
+  flexShrink: 0,
+  whiteSpace: "nowrap",
+  boxSizing: "border-box",
+  ...(open && {
+    ...openedMixin(theme),
+    "& .MuiDrawer-paper": openedMixin(theme),
+  }),
+  ...(!open && {
+    ...closedMixin(theme),
+    "& .MuiDrawer-paper": closedMixin(theme),
+  }),
+}));
 
 export const StyledBadge = styled(Badge)(({ theme }) => ({
-  '& .MuiBadge-badge': {
-    backgroundColor: '#44b700',
-    color: '#44b700',
+  "& .MuiBadge-badge": {
+    backgroundColor: "#44b700",
+    color: "#44b700",
     boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
-    '&::after': {
-      position: 'absolute',
+    "&::after": {
+      position: "absolute",
       top: 0,
       left: 0,
-      width: '100%',
-      height: '100%',
-      borderRadius: '50%',
-      animation: 'ripple 1.2s infinite ease-in-out',
-      border: '1px solid currentColor',
+      width: "100%",
+      height: "100%",
+      borderRadius: "50%",
+      animation: "ripple 1.2s infinite ease-in-out",
+      border: "1px solid currentColor",
       content: '""',
     },
   },
-  '@keyframes ripple': {
-    '0%': {
-      transform: 'scale(.8)',
+  "@keyframes ripple": {
+    "0%": {
+      transform: "scale(.8)",
       opacity: 1,
     },
-    '100%': {
-      transform: 'scale(2.4)',
+    "100%": {
+      transform: "scale(2.4)",
       opacity: 0,
     },
   },
 }));
 
 export default function Chat() {
-  const [roomId, setRoomId] = React.useState('');
+  const [isLanguageDialogOpen, setLanguageDialogOpen] = React.useState(false);
+  const [roomId, setRoomId] = React.useState("");
   const [selectedPerson, setSelectedPerson] = React.useState([]);
   const [messages, setMessages] = React.useState([]);
-  const [message, setMessage] = React.useState('');
+  const [message, setMessage] = React.useState("");
   const { open, setOpen } = useDrawer();
   const { user } = useUserAuth();
 
   const scrollToBottom = () => {
-    var objDiv = document.getElementById('boxData');
+    var objDiv = document.getElementById("boxData");
     if (objDiv) {
       objDiv.scrollTop = objDiv?.scrollHeight;
     }
@@ -150,7 +156,10 @@ export default function Chat() {
 
   function getMessages(roomId) {
     return onSnapshot(
-      query(collection(db, 'chats', roomId, 'messages'), orderBy('time', 'asc')),
+      query(
+        collection(db, "chats", roomId, "messages"),
+        orderBy("time", "asc")
+      ),
       (querySnapshot) => {
         const messages = querySnapshot.docs.map((doc) => {
           return {
@@ -170,7 +179,7 @@ export default function Chat() {
     setSelectedPerson(person);
     getMessages(roomid);
 
-    const chats_ref = doc(db, 'chats', roomid);
+    const chats_ref = doc(db, "chats", roomid);
     const myId = user.uid;
     const docSnap = await getDoc(chats_ref);
 
@@ -193,13 +202,16 @@ export default function Chat() {
   };
 
   const updateUnreadCount = async () => {
-    const chats_ref = doc(db, 'chats', roomId);
+    const chats_ref = doc(db, "chats", roomId);
     const partnerId = selectedPerson.data.uid;
     const docSnap = await getDoc(chats_ref);
 
     if (docSnap.exists()) {
       let roomDetail = docSnap.data();
-      let partnerUnreadCount = roomDetail && roomDetail[partnerId] && roomDetail[partnerId].unread_count;
+      let partnerUnreadCount =
+        roomDetail &&
+        roomDetail[partnerId] &&
+        roomDetail[partnerId].unread_count;
       updateDoc(chats_ref, {
         [partnerId]: {
           unread_count: partnerUnreadCount ? partnerUnreadCount + 1 : 1,
@@ -213,7 +225,7 @@ export default function Chat() {
           unread_count: 1,
         },
       }).then(() => {
-        console.log('unread count added');
+        console.log("unread count added");
       });
     }
   };
@@ -235,32 +247,68 @@ export default function Chat() {
         scrollToBottom();
       }, 100);
       try {
-        await addDoc(collection(db, 'chats', roomId, 'messages'), msgObj);
+        await addDoc(collection(db, "chats", roomId, "messages"), msgObj);
       } catch (error) {
         console.error(error);
       }
-      setMessage('');
+      setMessage("");
     } else {
-      setMessage('');
+      setMessage("");
+    }
+  };
+
+  const checkAndSetLanguage = async () => {
+    const userDocRef = doc(db, "users", user.uid);
+    const userDocSnap = await getDoc(userDocRef);
+
+    if (userDocSnap.exists()) {
+      const userData = userDocSnap.data();
+      if (userData.language === null) {
+        // Language is not set, open the language selection dialog
+        setLanguageDialogOpen(true);
+      }
+    }
+  };
+
+  React.useEffect(() => {
+    checkAndSetLanguage();
+  }, []);
+
+  const handleLanguageDialogClose = () => {
+    // Close the language selection dialog
+    setLanguageDialogOpen(false);
+  };
+
+  const handleLanguageSelection = async (selectedLanguage) => {
+    // Validate and update the user's language in Firestore
+    if (["English", "Hindi", "Marathi"].includes(selectedLanguage)) {
+      const userDocRef = doc(db, "users", user.uid);
+      await updateDoc(userDocRef, { language: selectedLanguage });
+      // Close the language selection dialog
+      setLanguageDialogOpen(false);
+    } else {
+      alert(
+        "Invalid language selection. Please choose from English, Hindi, or Marathi."
+      );
     }
   };
 
   return (
-    <Box sx={{ display: 'flex', height: '100%', position: 'relative' }}>
+    <Box sx={{ display: "flex", height: "100%", position: "relative" }}>
       <CssBaseline />
       <DrawerWithNav handlePersonChat={handlePersonChat} />
       <Box
         component="main"
         sx={(theme) => ({
           flexGrow: 1,
-          [theme.breakpoints.down('sm')]: {
-            marginLeft: '65px',
-            filter: open ? 'blur(50px)' : 'blur(0px)',
+          [theme.breakpoints.down("sm")]: {
+            marginLeft: "65px",
+            filter: open ? "blur(50px)" : "blur(0px)",
           },
-          position: 'relative',
-          overflow: 'hidden',
-          maxWidth: '1024px',
-          mx: 'auto',
+          position: "relative",
+          overflow: "hidden",
+          maxWidth: "1024px",
+          mx: "auto",
         })}
         onClick={() => setOpen(false)}
       >
@@ -269,13 +317,18 @@ export default function Chat() {
           <Box
             sx={(theme) => ({
               pt: 1,
-              [theme.breakpoints.down('lg')]: {
+              [theme.breakpoints.down("lg")]: {
                 px: 2,
               },
             })}
           >
             <CardHeader
-              avatar={<Avatar src={selectedPerson?.data?.profile_pictures} aria-label="recipe" />}
+              avatar={
+                <Avatar
+                  src={selectedPerson?.data?.profile_pictures}
+                  aria-label="recipe"
+                />
+              }
               sx={{ p: 0, mb: 0.5 }}
               title={selectedPerson?.data?.displayName}
             />
@@ -285,11 +338,11 @@ export default function Chat() {
         {selectedPerson?.data ? (
           <Box
             sx={{
-              height: 'calc(100vh - 197px)',
+              height: "calc(100vh - 197px)",
               pt: 1.5,
-              display: 'flex',
-              flexDirection: 'column',
-              overflow: 'auto',
+              display: "flex",
+              flexDirection: "column",
+              overflow: "auto",
             }}
             id="boxData"
           >
@@ -297,9 +350,9 @@ export default function Chat() {
               sx={{
                 flex: 1,
                 px: 3,
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'flex-end',
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "flex-end",
                 mb: 2,
               }}
               className="msgWrapper"
@@ -308,31 +361,43 @@ export default function Chat() {
                 return (
                   <Box
                     key={i}
-                    className={msg.sender === user.uid ? 'myMessage' : 'notMyMessage'}
+                    className={
+                      msg.sender === user.uid ? "myMessage" : "notMyMessage"
+                    }
                     sx={{
-                      alignSelf: msg.sender === user.uid ? 'end' : 'flex-start',
+                      alignSelf: msg.sender === user.uid ? "end" : "flex-start",
                       my: 0.3,
-                      maxWidth: '80%',
+                      maxWidth: "80%",
                     }}
                   >
                     <Chip
-                      color={msg.sender === user.uid ? 'primary' : 'secondary'}
+                      color={msg.sender === user.uid ? "primary" : "secondary"}
                       label={
                         <Box display="flex" flexDirection="column">
-                          <span style={{ fontWeight: '500' }}>{msg.message}</span>
-                          <Typography variant="body2" color="burlywood" fontWeight={'bold'}>
-                            {moment(msg.time.toDate().toString()).format('D-MMM-YY, h:mm a')}
+                          <span style={{ fontWeight: "500" }}>
+                            {msg.message}
+                          </span>
+                          <Typography
+                            variant="body2"
+                            color="burlywood"
+                            fontWeight={"bold"}
+                          >
+                            {moment(msg.time.toDate().toString()).format(
+                              "D-MMM-YY, h:mm a"
+                            )}
                           </Typography>
                         </Box>
                       }
                       sx={{
-                        fontSize: 'large',
-                        height: 'auto',
+                        fontSize: "large",
+                        height: "auto",
                         p: 1,
-                        borderTopRightRadius: msg.sender === user.uid ? '0px' : '16px',
-                        borderTopLeftRadius: msg.sender !== user.uid ? '0px' : '16px',
-                        '& span': {
-                          whiteSpace: 'normal',
+                        borderTopRightRadius:
+                          msg.sender === user.uid ? "0px" : "16px",
+                        borderTopLeftRadius:
+                          msg.sender !== user.uid ? "0px" : "16px",
+                        "& span": {
+                          whiteSpace: "normal",
                         },
                       }}
                     />
@@ -343,14 +408,22 @@ export default function Chat() {
           </Box>
         ) : null}
         {selectedPerson?.data ? (
-          <form onSubmit={sendMsg} style={{ position: 'sticky', bottom: 0, left: 0 }}>
-            <Box display="flex" sx={{ px: 1, alignItems: 'center', mb: 0.5, width: '100%' }}>
+          <form
+            onSubmit={sendMsg}
+            style={{ position: "sticky", bottom: 0, left: 0 }}
+          >
+            <Box
+              display="flex"
+              sx={{ px: 1, alignItems: "center", mb: 0.5, width: "100%" }}
+            >
               <Box sx={{ mr: 1, flex: 1 }}>
-                <FormControl sx={{ m: 1, width: '100%' }} variant="outlined">
-                  <InputLabel htmlFor="outlined-adornment-password">Message</InputLabel>
+                <FormControl sx={{ m: 1, width: "100%" }} variant="outlined">
+                  <InputLabel htmlFor="outlined-adornment-password">
+                    Message
+                  </InputLabel>
                   <OutlinedInput
                     value={message}
-                    sx={{ width: '100%' }}
+                    sx={{ width: "100%" }}
                     onChange={(e) => setMessage(e.target.value)}
                     endAdornment={
                       <InputAdornment position="end">
@@ -367,6 +440,23 @@ export default function Chat() {
           </form>
         ) : null}
       </Box>
+      <Dialog open={isLanguageDialogOpen} onClose={handleLanguageDialogClose}>
+        <DialogTitle>Select Your Language</DialogTitle>
+        <DialogContent>
+          <Button onClick={() => handleLanguageSelection("English")}>
+            English
+          </Button>
+          <Button onClick={() => handleLanguageSelection("Hindi")}>
+            Hindi
+          </Button>
+          <Button onClick={() => handleLanguageSelection("Marathi")}>
+            Marathi
+          </Button>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleLanguageDialogClose}>Cancel</Button>
+        </DialogActions>
+      </Dialog>
     </Box>
   );
 }
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,28 +1,34 @@
-import { Box, Card, CardContent, Typography } from '@mui/material';
-import React, { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import Layout from '../../components/Layout';
-import { useUserAuth } from '../../context/userAuthContext';
-import './Login.css';
+import { Box, Card, CardContent, Typography } from "@mui/material";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import Layout from "../../components/Layout";
+import { useUserAuth } from "../../context/userAuthContext";
+import "./Login.css";
 
 export default function Login() {
-  const { googleSignInWithPopup, user, detectMob, googleSignIn, githubSignIn, githubSignInWithPopup } =
-    useUserAuth();
+  const {
+    googleSignInWithPopup,
+    user,
+    detectMob,
+    googleSignIn,
+    // githubSignIn,
+    // githubSignInWithPopup,
+  } = useUserAuth();
   const navigate = useNavigate();
 
   const handleSignIn = async (e, provider) => {
     e.preventDefault();
     try {
       switch (provider) {
-        case 'google':
-          await detectMob() ? googleSignIn() : googleSignInWithPopup();
-          navigate('/chat');
+        case "google":
+          (await detectMob()) ? googleSignIn() : googleSignInWithPopup();
+          navigate("/chat");
           break;
 
-        case 'github':
-          await detectMob() ? githubSignIn() : githubSignInWithPopup();
-          navigate('/chat');
-          break;
+        // case 'github':
+        //   await detectMob() ? githubSignIn() : githubSignInWithPopup();
+        //   navigate('/chat');
+        //   break;
 
         default:
           break;
@@ -34,7 +40,7 @@ export default function Login() {
 
   useEffect(() => {
     if (user) {
-      navigate('/chat');
+      navigate("/chat");
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [user]);
@@ -42,24 +48,28 @@ export default function Login() {
   const card = (
     <React.Fragment>
       <CardContent>
-        <Typography variant='h4' color='text.secondary' gutterBottom>
+        <Typography variant="h4" color="text.secondary" gutterBottom>
           Penguins Chat
         </Typography>
-        <Box width='100%'>
-          <div className='google-btn' role={'button'} onClick={(e) => handleSignIn(e, 'google')}>
-            <div className='google-icon-wrapper'>
+        <Box width="100%">
+          <div
+            className="google-btn"
+            role={"button"}
+            onClick={(e) => handleSignIn(e, "google")}
+          >
+            <div className="google-icon-wrapper">
               <img
-                className='google-icon'
-                src='https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg'
-                alt='google'
+                className="google-icon"
+                src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+                alt="google"
               />
             </div>
-            <p className='btn-text'>
+            <p className="btn-text">
               <b>Login with Google</b>
             </p>
           </div>
         </Box>
-        <Box width='100%' mt={0.5}>
+        {/* <Box width='100%' mt={0.5}>
           <div className='google-btn' style={{ backgroundColor: "#03060a" }} role={'button'} onClick={(e) => handleSignIn(e, 'github')}>
             <div className='google-icon-wrapper'>
               <img className='google-icon' src='https://github.com/fluidicon.png' alt='Github' />
@@ -68,34 +78,34 @@ export default function Login() {
               <b>Login with Github</b>
             </p>
           </div>
-        </Box>
+        </Box> */}
       </CardContent>
     </React.Fragment>
   );
   return (
-    <Layout sx={{ height: '100vh', display: 'flex', alignItems: 'center' }}>
+    <Layout sx={{ height: "100vh", display: "flex", alignItems: "center" }}>
       <Box
         sx={{
-          padding: '16px',
-          backgroundColor: 'rgba(255, 255, 255, 0.1)',
-          inset: '0px',
-          maxWidth: '480px',
+          padding: "16px",
+          backgroundColor: "rgba(255, 255, 255, 0.1)",
+          inset: "0px",
+          maxWidth: "480px",
           // height: '100%',
-          overflowY: 'auto',
+          overflowY: "auto",
           // margin: '50px auto 0',
-          background: 'rgba( 255, 255, 255, 0.4 )',
-          boxShadow: '0 8px 32px 0 rgba(31, 38, 135, 0.37)',
-          backdropFilter: 'blur( 5px )',
-          WebkitBackdropFilter: 'blur( 5px )',
-          borderRadius: '10px',
-          border: '1px solid rgba(255, 255, 255, 0.18)',
+          background: "rgba( 255, 255, 255, 0.4 )",
+          boxShadow: "0 8px 32px 0 rgba(31, 38, 135, 0.37)",
+          backdropFilter: "blur( 5px )",
+          WebkitBackdropFilter: "blur( 5px )",
+          borderRadius: "10px",
+          border: "1px solid rgba(255, 255, 255, 0.18)",
           // marginBottom: '10px',
-          width: '100%',
+          width: "100%",
 
-          margin: 'auto',
+          margin: "auto",
         }}
       >
-        <Card variant='elevation' sx={{ width: '100%' }}>
+        <Card variant="elevation" sx={{ width: "100%" }}>
           {card}
         </Card>
       </Box>
